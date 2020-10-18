925a083eb407ac63cb6c2c45ec717572093387d2
nilubisan <r.nas9329@gmail.com>
2020-10-19 01:24:46 +0600

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrOfEncodSymbols = [];
+    for(let start = 0, end = 10 ; end <= expr.length; start+=10, end+=10) {
+        arrOfEncodSymbols.push(expr.substring(start,end));
+    }
+    let arrOfDecodSymbols = arrOfEncodSymbols.map(str => {
+        if(str[0] == 0) str = str.slice(str.indexOf(1));
+        if(str[0] == "*") {
+            return " ";
+        }
+    let strInitLength = str.length;
+    for(let i=0; i < strInitLength; i+=2) {
+        let code = str[i] + str[i+1];
+        str+= (code == "10" ? "." : "-");
+    }
+    return MORSE_TABLE[str.slice(strInitLength, str.length)];
+});
+    return arrOfDecodSymbols.join("");
 }
 
 module.exports = {
